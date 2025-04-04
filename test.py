0c6dc1f3c6b22ec09789461e54fcda071921f491
Alvin2805 <alvinsahroni@gmail.com>
2025-04-04 16:28:25 +0700

    test

--- test.py
+++ test.py
@@ -2,17 +2,19 @@ import streamlit as st
 import pandas as pd
 from st_aggrid import AgGrid, GridOptionsBuilder
 
-# @st.cache_data()
+st.set_page_config(page_title='IFR test report',layout='wide',initial_sidebar_state='expanded')
+
+@st.cache_data()
 def load_data():
-    data = pd.read_csv("./coba.csv", sep=";")
+    data = pd.read_excel("coba2.xlsx")
     return data
+
+
 
 data = load_data()
 print(data)
 
-st.set_page_config(page_title='IFR test report',layout='wide',initial_sidebar_state='expanded')
-
-shouldDisplayPivoted = st.checkbox("Pivot data on Reference Date")
+shouldDisplayPivoted = st.checkbox("Check the detail IFR")
 
 gb = GridOptionsBuilder()
 
@@ -23,38 +25,54 @@ gb.configure_default_column(
     editable=False,
 )
 gb.configure_column(
-    field="section", header_name="section", width=80, rowGroup=shouldDisplayPivoted
+    field="level_1", header_name="Level 1", width=80, rowGroup=shouldDisplayPivoted
 )
 
 gb.configure_column(
-    field="subsection",
-    header_name="subsection",
+    field="level_2",
+    header_name="Level 2",
     flex=1,
-    tooltipField="subsection",
+    tooltipField="Level 2",
     rowGroup=True if shouldDisplayPivoted else False,
 )
 
 gb.configure_column(
-    field="subsubsection",
-    header_name="subsubsection",
+    field="level_3",
+    header_name="Level 3",
     flex=1,
-    tooltipField="subsubsection",
+    tooltipField="Level 3",
     rowGroup=True if shouldDisplayPivoted else False,
 )
 
 gb.configure_column(
-    field="subsubsubsection",
-    header_name="subsubsubsection",
+    field="level_4",
+    header_name="Level 4",
     flex=1,
-    tooltipField="subsubsubsection",
+    tooltipField="Level 4",
     rowGroup=True if shouldDisplayPivoted else False,
 )
 
 gb.configure_column(
-    field="subsubsubsubsection",
-    header_name="subsubsubsubsection",
+    field="level_5",
+    header_name="Level 5",
     flex=1,
-    tooltipField="subsubsubsubsection",
+    tooltipField="Level 5",
+    rowGroup=True if shouldDisplayPivoted else False,
+)
+
+gb.configure_column(
+    field="level_6",
+    header_name="Level 6",
+    flex=1,
+    tooltipField="Level 6",
+    rowGroup=True if shouldDisplayPivoted else False,
+)
+
+gb.configure_column(
+    field="level_7",
+    header_name="Level 7",
+    flex=1,
+    tooltipField="Level 7",
     rowGroup=True if shouldDisplayPivoted else False,
 )
 
@@ -70,6 +88,30 @@ gb.configure_column(
 gb.configure_column(
     field="brand",
     header_name="brand",
+    # valueGetter="new Date(data.referenceDate).getFullYear()",
+    pivot=True,
+    hide=False,
+)
+
+gb.configure_column(
+    field="period_year",
+    header_name="Year",
+    # valueGetter="new Date(data.referenceDate).getFullYear()",
+    pivot=True,
+    hide=False,
+)
+
+gb.configure_column(
+    field="period_month",
+    header_name="Month",
+    # valueGetter="new Date(data.referenceDate).getFullYear()",
+    pivot=True,
+    hide=False,
+)
+
+gb.configure_column(
+    field="value_type",
+    header_name="Type",
     # valueGetter="new Date(data.referenceDate).getFullYear()",
     pivot=True,
     hide=False,
@@ -98,4 +140,4 @@ gb.configure_grid_options(
 )
 go = gb.build()
 
-AgGrid(data, gridOptions=go,fit_columns_on_grid_load=True,height=400)
+AgGrid(data, gridOptions=go,fit_columns_on_grid_load=True,height=700)
