e4f232f65f5910e628cf7f0262c606ca52d39b6d
alvinNomad2805 <alvinsahroni@gmail.com>
2025-04-06 10:54:41 +0700

    test

--- IFR.py
+++ IFR.py
@@ -4,140 +4,186 @@ from st_aggrid import AgGrid, GridOptionsBuilder
 
 st.set_page_config(page_title='IFR test report',layout='wide',initial_sidebar_state='expanded')
 
+st.title('Indomobil Financial Reports')
+
+c1,c2,c3,c4 = st.columns(4)
+
 @st.cache_data()
 def load_data():
     data = pd.read_excel("coba2.xlsx")
+    data.to_parquet('raw_data.parquet','auto')
     return data
 
-
-
 data = load_data()
-print(data)
 
-shouldDisplayPivoted = st.checkbox("Check the detail IFR")
+list_year = data['period_year'].unique().tolist()
+list_month = data['period_month'].unique().tolist()
+list_companies = data['company'].unique().tolist()
+list_brands = data['brand'].unique().tolist()
 
-gb = GridOptionsBuilder()
+with c1:
+    c1.selectbox('Period Year',list_year)
+with c2:
+    c2.selectbox('Period Month',list_month)
+with c3:
+    pass
+with c4:
+    pass
 
-gb.configure_default_column(
-    resizable=True,
-    filterable=True,
-    sortable=True,
-    editable=False,
-)
-gb.configure_column(
-    field="level_1", header_name="Level 1", width=80, rowGroup=shouldDisplayPivoted
-)
+st.multiselect('Select Company(ies) (multi selection)',list_companies)
+st.multiselect('Select Brand(s) (multi selection)',list_brands)
 
-gb.configure_column(
-    field="level_2",
-    header_name="Level 2",
-    flex=1,
-    tooltipField="Level 2",
-    rowGroup=True if shouldDisplayPivoted else False,
-)
+tabs = st.tabs(['Detail Summary','Detail Percentage'])
 
-gb.configure_column(
-    field="level_3",
-    header_name="Level 3",
-    flex=1,
-    tooltipField="Level 3",
-    rowGroup=True if shouldDisplayPivoted else False,
-)
+with tabs[0]:
+    st.subheader('IFR summary and detail')
+    shouldDisplayPivoted = st.checkbox("Check the detail IFR")
 
-gb.configure_column(
-    field="level_4",
-    header_name="Level 4",
-    flex=1,
-    tooltipField="Level 4",
-    rowGroup=True if shouldDisplayPivoted else False,
-)
+    gb = GridOptionsBuilder()
 
-gb.configure_column(
-    field="level_5",
-    header_name="Level 5",
-    flex=1,
-    tooltipField="Level 5",
-    rowGroup=True if shouldDisplayPivoted else False,
-)
-
-gb.configure_column(
-    field="level_6",
-    header_name="Level 6",
-    flex=1,
-    tooltipField="Level 6",
-    rowGroup=True if shouldDisplayPivoted else False,
-)
-
-gb.configure_column(
-    field="level_7",
-    header_name="Level 7",
-    flex=1,
-    tooltipField="Level 7",
-    rowGroup=True if shouldDisplayPivoted else False,
-)
-
-gb.configure_column(
-    field="company",
-    header_name="company",
-    width=100,
-    # valueFormatter="value != undefined ? new Date(value).toLocaleString('en-US', {dateStyle:'medium'}): ''",
-    pivot=True,
-    hide=False,
-)
-
-gb.configure_column(
-    field="brand",
-    header_name="brand",
-    # valueGetter="new Date(data.referenceDate).getFullYear()",
-    pivot=True,
-    hide=False,
-)
-
-gb.configure_column(
-    field="period_year",
-    header_name="Year",
-    # valueGetter="new Date(data.referenceDate).getFullYear()",
-    pivot=True,
-    hide=False,
-)
-
-gb.configure_column(
-    field="period_month",
-    header_name="Month",
-    # valueGetter="new Date(data.referenceDate).getFullYear()",
-    pivot=True,
-    hide=False,
-)
-
-gb.configure_column(
-    field="value_type",
-    header_name="Type",
-    # valueGetter="new Date(data.referenceDate).getFullYear()",
-    pivot=True,
-    hide=False,
-)
-
-gb.configure_column(
-    field="total",
-    header_name="total",
-    width=100,
-    type=["numericColumn"],
-    aggFunc="sum",
-    # valueFormatter="value.toLocaleString()",
-)
-
-gb.configure_grid_options(
-    tooltipShowDelay=0,
-    pivotMode=shouldDisplayPivoted,
-)
-
-gb.configure_grid_options(
-    autoGroupColumnDef=dict(
-        minWidth=300, 
-        pinned="left", 
-        cellRendererParams=dict(suppressCount=True)
+    gb.configure_default_column(
+        resizable=True,
+        filterable=True,
+        sortable=True,
+        editable=False,
     )
-)
-go = gb.build()
+    gb.configure_column(
+        field="level_1", header_name="Level 1", width=80, rowGroup=shouldDisplayPivoted
+    )
 
-AgGrid(data, gridOptions=go,fit_columns_on_grid_load=True,height=700)
+    gb.configure_column(
+        field="level_2",
+        header_name="Level 2",
+        flex=1,
+        tooltipField="Level 2",
+        rowGroup=True if shouldDisplayPivoted else False,
+    )
+
+    gb.configure_column(
+        field="level_3",
+        header_name="Level 3",
+        flex=1,
+        tooltipField="Level 3",
+        rowGroup=True if shouldDisplayPivoted else False,
+    )
+
+    gb.configure_column(
+        field="level_4",
+        header_name="Level 4",
+        flex=1,
+        tooltipField="Level 4",
+        rowGroup=True if shouldDisplayPivoted else False,
+    )
+
+    gb.configure_column(
+        field="level_5",
+        header_name="Level 5",
+        flex=1,
+        tooltipField="Level 5",
+        rowGroup=True if shouldDisplayPivoted else False,
+    )
+
+    gb.configure_column(
+        field="level_6",
+        header_name="Level 6",
+        flex=1,
+        tooltipField="Level 6",
+        rowGroup=True if shouldDisplayPivoted else False,
+    )
+
+    gb.configure_column(
+        field="level_7",
+        header_name="Level 7",
+        flex=1,
+        tooltipField="Level 7",
+        rowGroup=True if shouldDisplayPivoted else False,
+    )
+
+    gb.configure_column(
+        field="company",
+        header_name="company",
+        width=100,
+        # valueFormatter="value != undefined ? new Date(value).toLocaleString('en-US', {dateStyle:'medium'}): ''",
+        pivot=True,
+        hide=False,
+    )
+
+    gb.configure_column(
+        field="brand",
+        header_name="brand",
+        # valueGetter="new Date(data.referenceDate).getFullYear()",
+        pivot=True,
+        hide=False,
+    )
+
+    gb.configure_column(
+        field="period_year",
+        header_name="Year",
+        # valueGetter="new Date(data.referenceDate).getFullYear()",
+        pivot=False,
+        hide=False,
+    )
+
+    gb.configure_column(
+        field="period_month",
+        header_name="Month",
+        # valueGetter="new Date(data.referenceDate).getFullYear()",
+        pivot=False,
+        hide=False,
+    )
+
+    gb.configure_column(
+        field="value_type",
+        header_name="Type",
+        rowGroup=True,
+        # valueGetter="new Date(data.referenceDate).getFullYear()",
+        pivot=True,
+        hide=False,
+    )
+
+    gb.configure_column(
+        field="value",
+        header_name="Total",
+        width=100,
+        type=["numericColumn"],
+        aggFunc="sum",
+        # valueFormatter="value.toLocaleString()",
+    )
+
+    gb.configure_column(
+        field="percentage",
+        header_name="Percentage",
+        width=100,
+        type=["numericColumn"],
+        # valueFormatter="value.toLocaleString()",
+    )
+
+
+    gb.configure_grid_options(
+        tooltipShowDelay=0,
+        pivotMode=shouldDisplayPivoted,
+    )
+
+    gb.configure_grid_options(
+        autoGroupColumnDef=dict(
+            minWidth=300, 
+            pinned="left", 
+            cellRendererParams=dict(suppressCount=True)
+        )
+    )
+    go = gb.build()
+
+    AgGrid(data, gridOptions=go,fit_columns_on_grid_load=True,height=700)
+with tabs[1]:
+    st.subheader('Percentage Summary')
+    data = {
+        "Group":['Unit Sales'],
+        "Company":["Indomobil Trada Nasional - MT Haryono"],
+        "Brand":["Nissan"],
+        "Year":["2024"],
+        "Month":["04"],
+        "Percentage":[43]
+
+    }
+    df = pd.DataFrame(data=data)
+    st.dataframe(df)
