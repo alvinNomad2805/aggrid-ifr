054eba0709abbbe763ca78bc75ca980cb1fd49b1
Alvin2805 <alvinsahroni@gmail.com>
2025-04-09 09:19:24 +0700

    ifr new development and deployment

--- scheduler.py
+++
@@ -1,8 +0,0 @@
-import requests
-import pandas as pd
-
-
-url = requests.get('http://172.16.5.5:5000/ifr-new-format')
-data = url.json()['Data']
-data_complete = pd.DataFrame(data)
-data_to_parquet = data_complete.to_parquet('raw_data.parquet')
---
+++ scheduler/scheduler.py
@@ -0,0 +1,17 @@
+import requests
+import pandas as pd
+import time
+
+
+if __name__ == '__main__':
+    try:
+        while (True):
+            print("getting new data --- refreshed (Ctrl + C to terminate)")
+            url = requests.get('http://172.16.5.5:5000/ifr-new-format')
+            data = url.json()['Data']
+            data_complete = pd.DataFrame(data)
+            data_to_parquet = data_complete.to_parquet('raw_data.parquet')
+            time.sleep(86400)
+            print('finished....')
+    except KeyboardInterrupt:
+        print('scheduler terminated...')
